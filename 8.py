a103c6f68165ed177a98fa5b4a3de81f6a537189
dasha <144099222+Juggufu@users.noreply.github.com>
2023-12-01 16:58:38 +0900

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,29 @@
+def main():
+    #  1
+    # all_num = set()
+    # for i in range(int(input())):
+    #     all_num.add(int(input()))
+    # new_num = set()
+    # for element_1 in all_num:
+    #     for element_2 in all_num:
+    #         new_num.add(element_1 - element_2)
+    # new_num = sorted(list(new_num))
+    # print(*new_num, sep='\n')
+    # #  2
+    # a = 1000000000
+    # bank = list()
+    # over = list()
+    # tenge = input()
+    # over = list(map(int, tenge.split(',')))
+    # for i in over:
+    #     if i > a:
+    #         bank.append(i)
+    # print(bank)
+    #  3
+    # a = input()
+    # for i in range(len(a)):
+    #     print(2*a[i], end='')
+
+
+if __name__ == '__main__':
+    main()
