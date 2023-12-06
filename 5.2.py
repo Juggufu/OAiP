a27a25006ad7ceb9b99b90d0124fbb971391e234
dasha <144099222+Juggufu@users.noreply.github.com>
2023-12-06 12:20:33 +0900

    Update 5.2.py

--- 5.2.py
+++ 5.2.py
@@ -1,57 +1,57 @@
 def main():
-    # 1
-    # c = ''
-    # b = int(input())
-    # for i in range(b):
-    #     a = input()
-    #     c += a
-    # print(len(set(c)))
+    #1
+    c = ''
+    b = int(input())
+    for i in range(b):
+        a = input()
+        c += a
+    print(len(set(c)))
 
-    # 2
-    # c = set()
-    # f = set()
-    # for i in range(3):
-    #     a = str.split(input())
-    #     c = a
-    # print(c)
+    #2
+    c = set()
+    f = set()
+    for i in range(3):
+        a = str.split(input())
+        c = a
+    print(c)
 
-    # 3
-    # c = set()
-    # b = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-    # a = set(input())
-    # print(a)
-    # y = b.symmetric_difference(a)
-    # print(*y)
+    #3
+    c = set()
+    b = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    a = set(input())
+    print(a)
+    y = b.symmetric_difference(a)
+    print(*y)
 
-    # 4
-    # sequence_number = 0
-    # share = set()
-    # entering_numbers = int(input())
-    # while entering_numbers != '':
-    #     sequence_number += 1
-    #     if entering_numbers // sequence_number == 0:
-    #         share.add(entering_numbers)
-    #     else:
-    #         share.add(0)
-    # entering_numbers = int(input())
-    # print(share)
+   # 4
+    sequence_number = 0
+    share = set()
+    entering_numbers = int(input())
+    while entering_numbers != '':
+        sequence_number += 1
+        if entering_numbers // sequence_number == 0:
+            share.add(entering_numbers)
+        else:
+            share.add(0)
+    entering_numbers = int(input())
+    print(share)
 
-    # 5
-    # all_colors = []
-    # number_of_colors = int(input())
-    # for i in range(number_of_colors):
-    #     color = input()
-    #     all_colors.append(color)
-    # number_of_repeat = int(input())
-    # for c in range(number_of_repeat):
-    #     print(all_colors)
+   # 5
+    all_colors = []
+    number_of_colors = int(input())
+    for i in range(number_of_colors):
+        color = input()
+        all_colors.append(color)
+    number_of_repeat = int(input())
+    for c in range(number_of_repeat):
+        print(all_colors)
 
-    # 7
-    # bert = set()
-    # bears = input()
-    # while bears != '':
-    #     bert.add(bears)
-    # print(bert)
+    #7
+    bert = set()
+    bears = input()
+    while bears != '':
+        bert.add(bears)
+    print(bert)
 
     # 8
     digits = {}
