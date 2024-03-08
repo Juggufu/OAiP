1059054693a9484ebe27fad63d055afdde2f747d
dasha <144099222+Juggufu@users.noreply.github.com>
2024-03-08 10:40:39 +0900

    Add files via upload

---
+++ лб7/2спи_щеглова_л7.py
@@ -0,0 +1,16 @@
+from factorial import factorial
+from fibonacci import fibonacci
+from vowels import vowels
+from check import check
+
+
+def main():
+    print(factorial(5))
+    print(fibonacci(5))
+    print(vowels('прикет как дела пойдем гулять'))
+    print(check(5))
+
+
+if __name__ == '__main__':
+    main()
+# 5
